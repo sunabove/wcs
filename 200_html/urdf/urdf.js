f88723a77df88d4312c06035371b4073093a9525
sunabove <sunabove@nate.com>
2026-04-08 15:22:44 +0900

    **

--- 200_html/urdf/urdf.js
+++ 200_html/urdf/urdf.js
@@ -145,7 +145,7 @@ class URDFViewer {
                     let cameraPosition;
 
                     cameraPosition = {
-                                x: center.x + cameraDist * 0.2,
+                                x: center.x + cameraDist * 0.1,
                                 y: center.y + cameraDist * 1.2,
                                 z: center.z - cameraDist * 0.8
                             };

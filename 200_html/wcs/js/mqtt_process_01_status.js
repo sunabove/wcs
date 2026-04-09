1cc185279d11bd0a442ecefcfa5bcf97345a6c68
sunabove <sunabove@nate.com>
2026-04-09 15:51:46 +0900

    **

--- 200_html/wcs/js/mqtt_process_01_status.js
+++ 200_html/wcs/js/mqtt_process_01_status.js
@@ -60,8 +60,8 @@ function prcessMqttMessage(topic, value) {
             .addClass('disabled')
             .css({ 
                 'opacity': '0.6', 
-                'color': '#999',
-                'background-color': '#666'  // Light gray 배경색
+                'color': '#666',
+                'background-color': '#999'  // Light gray 배경색
             });
         
         // 해당 노면 상태 요소에 테두리 추가 및 활성화

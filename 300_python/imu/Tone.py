de39999166b8ec5c92ca84d1b344f26545d39e2f
sunabove <sunabove@nate.com>
2026-06-18 14:36:57 +0900

    **

--- 300_python/imu/Tone.py
+++ 300_python/imu/Tone.py
@@ -3,9 +3,10 @@ from gpiozero.tones import Tone
 
 buzzer = TonalBuzzer(4)
 
-print("Playing tone A4...")
+tone = "A2"
+print(f"Playing tone {tone}...")
 
-buzzer.play(Tone("A3"))
+buzzer.play(Tone(tone))
 
 # 사용자 입력을 기다림으로써 프로그램이 종료되지 않도록 함
 input("Enter to quit! ")

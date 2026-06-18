8bf9b3ed5694d2963ea9834e88c7c2f8a39396c0
sunabove <sunabove@nate.com>
2026-06-18 14:40:46 +0900

    **

---
+++ 300_python/imu/Siren.py
@@ -0,0 +1,14 @@
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+from time import sleep
+
+buzzer = TonalBuzzer(4)
+
+for _ in range(10):
+    buzzer.play(Tone("A4"))
+    sleep(0.2)
+
+    buzzer.play(Tone("A5"))
+    sleep(0.2)
+
+buzzer.stop()

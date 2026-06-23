44a10301043e1933af19b2a114d76073d609d16d
sunabove <sunabove@nate.com>
2026-06-23 13:52:43 +0900

    **

--- 300_python/imu/Encoder.py
+++ 300_python/imu/Encoder.py
@@ -10,24 +10,27 @@ class Encoder:
 	def __init__(self, gpio_no, name="Encoder"):
 		self.name = name
 		self.count = 0
+  
 		self.inputDevice = DigitalInputDevice(gpio_no, bounce_time=0.005)
 		self.inputDevice.when_activated = self._when_activated
 		self.inputDevice.when_deactivated = self._when_deactivated
-		print(f"{self.name} monitor started")
+  
+		print(f"{self.name} encoder started")
+  
 		self.print_status()
 	pass # __init__
 
 	def _when_activated(self):
 		self.count += 1
-		print(f"{self.name} detected (count={self.count})")
+		print(f"{self.name} encoder detected (count={self.count})")
 	pass # _when_activated
 
 	def _when_deactivated(self):
-		print(f"{self.name} released")
+		print(f"{self.name} encoder released")
 	pass # _when_deactivated
 
 	def print_status(self):
-		print(f"{self.name}: {'ACTIVE' if self.inputDevice.is_active else 'INACTIVE'}")
+		print(f"{self.name} encoder: {'ACTIVE' if self.inputDevice.is_active else 'INACTIVE'}")
 	pass # print_status
 
 	def close(self):
@@ -38,13 +41,19 @@ pass # Encoder
 
 
 def main():
-	monitor = Encoder(GpioNo.ENCODER_FR, "FR")
+	encoderFR = Encoder(GpioNo.ENCODER_FR, "FR")
+	encoderFL = Encoder(GpioNo.ENCODER_FL, "FL")
+	encoderRR = Encoder(GpioNo.ENCODER_RR, "RR")
+	encoderRL = Encoder(GpioNo.ENCODER_RL, "RL") 
 	try:
-		pause()
+		input("Enter to quit! ")
 	except KeyboardInterrupt:
 		pass
 	finally:
-		monitor.close()
+		encoderFR.close()
+		encoderFL.close()
+		encoderRR.close()
+		encoderRL.close()
 	pass # main
 
 

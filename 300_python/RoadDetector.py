e86a863e37f97ccffe4f962dfed8cce03b5589d2
sunabove <sunabove@nate.com>
2026-06-25 09:24:14 +0900

    **

--- 300_python/RoadDetector.py
+++ 300_python/RoadDetector.py
@@ -17,9 +17,12 @@ class RoadDetector:
     _class_color_map_path = Path(__file__).resolve().parent / "colormap_road.txt"
     _class_color_map = None
     
+    road_type_model_name = "ai/road/model/03_yolo11m-cobot-road-type-sg.pt"
+    #road_type_model_name = "ai/road/model/03_yolo11m-road-type-sg.pt"
+    
     _model_paths = {
         "road": Path(__file__).resolve().parent / "ai/road/model/01_yolo11m-road-sg.pt",
-        "road_type": Path(__file__).resolve().parent / "ai/road/model/02_yolo11m-road-type-sg.pt",
+        "road_type": Path(__file__).resolve().parent / road_type_model_name ,
         "pothole": Path(__file__).resolve().parent / "ai/road/model/04_yolo11m-pothole-sg.pt",
     }
     _models = {}

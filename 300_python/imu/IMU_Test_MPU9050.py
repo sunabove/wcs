4796eef3cc9dd8e4b37c2af0f7bbb5dc786585a4
sunabove <sunabove@nate.com>
2026-06-29 10:53:58 +0900

    **

--- 300_python/imu/IMU_Test_MPU9050.py
+++ 300_python/imu/IMU_Test_MPU9050.py
@@ -146,8 +146,8 @@ def main():
         f"(Mag={accel_baseline_mag:.3f} g)\n"
     )
     print(
-        f"Gyro baseline vector : X={gyro_baseline[0]:.3f} dps, "
-        f"Y={gyro_baseline[1]:.3f} dps, Z={gyro_baseline[2]:.3f} dps\n"
+        f"Gyro baseline vector : X={gyro_baseline[0]:.3f} °/s, "
+        f"Y={gyro_baseline[1]:.3f} °/s, Z={gyro_baseline[2]:.3f} °/s\n"
     )
     
     while True:
@@ -181,10 +181,10 @@ def main():
             gyro_c_axis = mat_vec_mul(rot_to_z, gyro_c)
             
             print(line)
-            print(f"[{count:4d}] {('Acce-C'):<{label_width}} : X: {accel_c_axis[0]:{value_width}.2f} g | Y: {accel_c_axis[1]:{value_width}.2f} g | Z: {accel_c_axis[2]:{value_width}.2f} g | CMag: {accel_c_mag:{value_width}.2f} g")
-            print(f"[{count:4d}] {('Acce-R'):<{label_width}} : X: {accel[0]:{value_width}.2f} g | Y: {accel[1]:{value_width}.2f} g | Z: {accel[2]:{value_width}.2f} g | RawMag: {accel_mag:{value_width}.2f} g")
-            print(f"[{count:4d}] {('Gyro-C'):<{label_width}} : X: {gyro_c_axis[0]:{value_width}.2f} dps | Y: {gyro_c_axis[1]:{value_width}.2f} dps | Z: {gyro_c_axis[2]:{value_width}.2f} dps | CMag: {gyro_c_mag:{value_width}.2f} dps")
-            print(f"[{count:4d}] {('Gyro-R'):<{label_width}} : X: {gyro[0]:{value_width}.2f} dps | Y: {gyro[1]:{value_width}.2f} dps | Z: {gyro[2]:{value_width}.2f} dps | RawMag: {gyro_mag:{value_width}.2f} dps")
+            print(f"[{count:4d}] {('Acce-R'):<{label_width}} : X: {accel[0]:{value_width}.2f} g | Y: {accel[1]:{value_width}.2f} g | Z: {accel[2]:{value_width}.2f} g | Mag-R: {accel_mag:{value_width}.2f} g")
+            print(f"[{count:4d}] {('Acce-C'):<{label_width}} : X: {accel_c_axis[0]:{value_width}.2f} g | Y: {accel_c_axis[1]:{value_width}.2f} g | Z: {accel_c_axis[2]:{value_width}.2f} g | Mag-C: {accel_c_mag:{value_width}.2f} g")
+            print(f"[{count:4d}] {('Gyro-R'):<{label_width}} : X: {gyro[0]:{value_width}.2f} °/s | Y: {gyro[1]:{value_width}.2f} °/s | Z: {gyro[2]:{value_width}.2f} °/s | Mag-R: {gyro_mag:{value_width}.2f} °/s")
+            print(f"[{count:4d}] {('Gyro-C'):<{label_width}} : X: {gyro_c_axis[0]:{value_width}.2f} °/s | Y: {gyro_c_axis[1]:{value_width}.2f} °/s | Z: {gyro_c_axis[2]:{value_width}.2f} °/s | Mag-C: {gyro_c_mag:{value_width}.2f} °/s")
             print(line)
 
             time.sleep(0.5) # Refresh rate delay

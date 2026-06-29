d22e253fe6387d4d612ee50955d971d8cd9ede97
sunabove <sunabove@nate.com>
2026-06-29 15:21:15 +0900

    **

--- 300_python/imu/IMU_9250.py
+++ 300_python/imu/IMU_9250.py
@@ -6,6 +6,7 @@ import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
 from mpu9250_jmdev.registers import WHO_AM_I
+from mpu9250_jmdev.registers import AK8963_CNTL1 
 
 class IMU9250App:
     def __init__(
@@ -28,15 +29,14 @@ class IMU9250App:
 
     def initialize_sensor(self):
         self.imu = MPU9250(
-            address_ak=AK8963_ADDRESS,
             address_mpu_master=MPU9050_ADDRESS_68,
             address_mpu_slave=None,
             bus=1,
             gfs=GFS_250,
             afs=AFS_2G,
-            mfs=AK8963_BIT_16,
-            mode=AK8963_MODE_C100HZ,
         )
+        
+        self.imu.writeAK(AK8963_CNTL1, 0x00)
         
         self.imu.configure()
 

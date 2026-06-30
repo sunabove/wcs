e3a82a3044abddabfe6226a05c7a88ef43e31b71
sunabove <sunabove@nate.com>
2026-06-30 14:54:11 +0900

    **

--- 300_python/imu/IMU_ICM20602_02_Check.py
+++ 300_python/imu/IMU_ICM20602_02_Check.py
@@ -2,9 +2,13 @@ from smbus2 import SMBus
 
 bus = SMBus(1)
 
-print(hex(bus.read_byte_data(0x69, 0x75)))   # WHO_AM_I
+print( "whoami = ", hex(bus.read_byte_data(0x69, 0x75)))   # WHO_AM_I
 
+input("Press Enter to continue...")
+
+cnt = 1
 while True:
     data = bus.read_i2c_block_data(0x69, 0x3B, 14)
-    print(data)
+    print( f"[{cnt:5d}] {data}")
+    cnt += 1
 pass 

051ddb9c79de72206663a3f2a25f3b266c1fcbdb
sunabove <sunabove@nate.com>
2026-06-30 14:27:48 +0900

    **

--- 300_python/imu/IMU_ICM20602.py
+++ 300_python/imu/IMU_ICM20602.py
@@ -3,29 +3,6 @@ from math import trunc
 from icm20602 import ICM20602
 from time import sleep
 
-
-mpu = ICM20602()
-print(mpu.check_availability(verbose=True))
-mpu.close()
-mpu=None
-
-input("Press Enter to continue...")
-
-print("---")
-
-print("Raw example")
-mpu = ICM20602()
-for _ in range(5):
-    accel_g = mpu.read_accel_data()
-    gyro_g = mpu.read_gyro_data()
-    print(f"Accel: {accel_g}, Gyro: {gyro_g}")
-    sleep(0.1)
-mpu.close()
-mpu=None
-
-input("Press Enter to continue...")
-print("---")
-
 # smoothing + low pass filter
 mpu = ICM20602()
 mpu.enable_smoothing(smoothing_window=7)
@@ -64,13 +41,22 @@ print("---")
 
 # continious reading
 mpu = ICM20602()
+mpu.calibrate_sensor()
+mpu.enable_smoothing(smoothing_window=7)
+mpu.enable_dlpf(bandwidth=mpu.DLPFBandwidth.BW_20HZ)
 print("Continous reading, break to stop")
 cnt = 1
-while True:
-    accel_g = mpu.read_accel_data()
-    roll, pitch = mpu.calculate_inclination(accel_g)
-    print(f"[{cnt:5d}] roll: {roll:.2f}, pitch: {pitch:.2f}")
-    sleep(0.25)
-    cnt += 1
-mpu.close()
-mpu = None
+try:
+    while True:
+        accel_g = mpu.read_accel_data()
+        gyro_g = mpu.read_gyro_data()
+        roll, pitch = mpu.calculate_inclination(accel_g)
+        print(f"[{cnt:5d}] Accel: {accel_g}, Gyro: {gyro_g}")
+        print(f"[{cnt:5d}] roll: {roll:.2f}, pitch: {pitch:.2f}")
+        sleep(0.25)
+        cnt += 1
+except KeyboardInterrupt:
+    print("\nStopped by Ctrl-C")
+finally:
+    mpu.close()
+    mpu = None

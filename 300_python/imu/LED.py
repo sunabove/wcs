0b50a15a16c169ac08f0f96a00b3a464b4de762e
sunabove <sunabove@nate.com>
2026-06-30 13:43:34 +0900

    **

--- 300_python/imu/LED.py
+++ 300_python/imu/LED.py
@@ -29,7 +29,24 @@ class LED :
             addr=0x3C,
         )
         self.font = ImageFont.load_default()
+        self.large_font = self._load_large_font(18)
     pass  # __init__
+
+    def _load_large_font(self, size):
+        font_candidates = [
+            "DejaVuSans.ttf",
+            "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
+            "/usr/share/fonts/truetype/freefont/FreeSans.ttf",
+        ]
+
+        for font_path in font_candidates:
+            try:
+                return ImageFont.truetype(font_path, size=size)
+            except OSError:
+                continue
+
+        return self.font
+    pass  # _load_large_font
 
     def clear(self):
         self.oled.fill(0)
@@ -47,6 +64,28 @@ class LED :
         visible_lines = [str(line)[:20] for line in lines[:max_lines]]
 
         line_count = len(visible_lines)
+
+        if line_count == 1:
+            line = visible_lines[0]
+            box = draw.textbbox((0, 0), line, font=self.large_font)
+            line_width = box[2] - box[0]
+            max_text_width = max(1, self.width - (2 * self.margin_x))
+
+            while len(line) > 1 and line_width > max_text_width:
+                line = line[:-1]
+                box = draw.textbbox((0, 0), line, font=self.large_font)
+                line_width = box[2] - box[0]
+
+            line_height = box[3] - box[1]
+            usable_height = max(1, self.height - (2 * self.margin_y))
+            x = max(self.margin_x, (self.width - line_width) // 2)
+            y = self.margin_y + max(0, (usable_height - line_height) // 2)
+
+            draw.text((x, y - box[1]), line, font=self.large_font, fill=255)
+            self.oled.image(image)
+            self.oled.show()
+            return
+
         line_boxes = [draw.textbbox((0, 0), line, font=self.font) for line in visible_lines]
         line_heights = [box[3] - box[1] for box in line_boxes]
         usable_height = max(1, self.height - (2 * self.margin_y))

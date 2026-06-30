66a2b81b82a2a9b54a24422d1d73e60f6b001843
sunabove <sunabove@nate.com>
2026-06-30 14:33:13 +0900

    **

--- 300_python/imu/IMU_ICM20602.py
+++ 300_python/imu/IMU_ICM20602.py
@@ -8,31 +8,14 @@ mpu = ICM20602()
 mpu.enable_smoothing(smoothing_window=7)
 mpu.enable_dlpf(bandwidth=mpu.DLPFBandwidth.BW_20HZ)
 print("Smoothing + DLPF example")
-for _ in range(5):
-    accel_g = mpu.read_accel_data()
-    gyro_g = mpu.read_gyro_data()
-    print(f"Accel: {accel_g}, Gyro: {gyro_g}")
-    sleep(0.1)
-mpu.close()
-mpu=None
-
-input("Press Enter to continue...")
-print("---")
-
-# calibration example
-# beware, calibrations sets all to 0,
-# if not placed correctly, the initial readings will not be exactly as expected
-
-mpu = ICM20602()
-mpu.calibrate_sensor()
-mpu.enable_smoothing(smoothing_window=7)
-mpu.enable_dlpf(bandwidth=mpu.DLPFBandwidth.BW_20HZ)
-print("Calibration + Smoothing + DLPF example")
-for _ in range(5):
-    accel_g = mpu.read_accel_data()
-    gyro_g = mpu.read_gyro_data()
-    print(f"Accel: {accel_g}, Gyro: {gyro_g}")
-    sleep(0.1)
+try:
+    while True:
+        accel_g = mpu.read_accel_data()
+        gyro_g = mpu.read_gyro_data()
+        print(f"Accel: {accel_g}, Gyro: {gyro_g}")
+        sleep(0.1)
+except KeyboardInterrupt:
+    print("Stopped by user")
 mpu.close()
 mpu=None
 
@@ -51,8 +34,8 @@ try:
         accel_g = mpu.read_accel_data()
         gyro_g = mpu.read_gyro_data()
         roll, pitch = mpu.calculate_inclination(accel_g)
-        print( f"[{cnt:5d}] Accel: {accel_g}, Gyro: {gyro_g} " 
-               f"[{cnt:5d}] roll: {roll:.2f}, pitch: {pitch:.2f}"
+        print( f"[{cnt:5d}] Accel: {accel_g}, Gyro: {gyro_g}" 
+               f", roll: {roll:.2f}, pitch: {pitch:.2f}"
               )
         sleep(0.25)
         cnt += 1

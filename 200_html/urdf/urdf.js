0535186ee4299ce3ed876dfa8868543fea55f674
sunabove <sunabove@nate.com>
2026-07-02 17:41:10 +0900

    ㅁㅁ

--- 200_html/urdf/urdf.js
+++ 200_html/urdf/urdf.js
@@ -408,7 +408,7 @@ class URDFViewer {
         const px = formatPositionValue(this.camera.position.x);
         const py = formatPositionValue(this.camera.position.y);
         const pz = formatPositionValue(this.camera.position.z);
-        const positionText = `x: ${px}, y: ${py}, z: ${pz}`;
+        const positionText = `(${px}, ${py}, ${pz})`;
 
         if (this.cameraPosTextElement && this.cameraPosTextElement.length > 0) {
             this.cameraPosTextElement.text(positionText);

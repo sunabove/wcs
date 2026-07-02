639a3212b071836c300b4597cf275f3435627982
sunabove <sunabove@nate.com>
2026-07-02 10:33:19 +0900

    ㅁㅁ

--- 200_html/urdf/urdf.js
+++ 200_html/urdf/urdf.js
@@ -397,7 +397,7 @@ class URDFViewer {
             const normalized = Math.abs(value).toFixed(fractionDigits);
             const [integerPart, fractionPart] = normalized.split('.');
             const signText = value < 0 ? '-' : '';
-            const paddedIntegerPart = integerPart.padStart(integerDigits, ' ');
+            const paddedIntegerPart = integerPart.padStart(integerDigits, '0');
             return `${signText}${paddedIntegerPart}.${fractionPart}`;
         };
 

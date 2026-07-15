0cefdd24e09a4ba5d0aa087e7114b99478e4e938
sunabove <sunabove@nate.com>
2026-07-15 14:02:56 +0900

    ㅁㅁ

--- 300_python/sam2/Sam2VideoConfig.py
+++ 300_python/sam2/Sam2VideoConfig.py
@@ -4,7 +4,8 @@ from config import BASE_DIR
 
 # SAM2 uses promptable segmentation checkpoints from the Meta SAM2 family.
 # The installed `sam2` package expects a Hugging Face model ID here.
-SAM2_DEFAULT_MODEL = "facebook/sam2.1-hiera-tiny"
+#SAM2_DEFAULT_MODEL = "facebook/sam2.1-hiera-tiny"
+SAM2_DEFAULT_MODEL = "facebook/sam2.1_hiera_small"
 SAM2_UPLOAD_SUBDIR = Path("upload/sam2/input")
 SAM2_OUTPUT_SUBDIR = Path("upload/sam2/output")
 

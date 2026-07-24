4b9f98fcffc9bcd581802961670ef4f9df4bd499
sunabove <sunabove@nate.com>
2026-07-24 10:00:54 +0900

    ㅁ

--- 200_html/urdf/urdfViewer.js
+++ 200_html/urdf/urdfViewer.js
@@ -736,6 +736,7 @@ class URDFViewer {
         compassRenderer.domElement.style.width = '48px';
         compassRenderer.domElement.style.height = '48px';
         compassRenderer.domElement.style.display = 'block';
+        compassRenderer.domElement.style.cursor = 'pointer';
         viewportElement.appendChild(compassRenderer.domElement);
 
         const compassScene = new THREE.Scene();

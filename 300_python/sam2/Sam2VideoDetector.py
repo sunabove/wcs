0e134ff525b9854891be0618026a54ad960bda65
sunabove <sunabove@nate.com>
2026-08-12 10:28:47 +0900

    ㅁㅁ

--- 300_python/sam2/Sam2VideoDetector.py
+++ 300_python/sam2/Sam2VideoDetector.py
@@ -1296,10 +1296,13 @@ class Sam2VideoDetector:
                 cv2.LINE_AA,
             )
 
-        cv2.line(canvas, (current_x - 1, chart_y1), (current_x - 1, chart_y2), (235, 235, 235), 5, cv2.LINE_AA)
-        cv2.line(canvas, (current_x, chart_y1), (current_x, chart_y2), (235, 235, 235), 2, cv2.LINE_AA)
-        cv2.line(canvas, (reference_x + 1, chart_y1), (reference_x + 1, chart_y2), (255, 180, 80), 2, cv2.LINE_AA)
-        cv2.line(canvas, (reference_x, chart_y1), (reference_x, chart_y2), (255, 180, 80), 1, cv2.LINE_AA)
+        ref_thickness = 2
+        curr_thickness = min(ref_thickness, 2)
+
+        cv2.line(canvas, (current_x - 1, chart_y1), (current_x - 1, chart_y2), (235, 235, 235), curr_thickness + 2, cv2.LINE_AA)
+        cv2.line(canvas, (current_x, chart_y1), (current_x, chart_y2), (235, 235, 235), curr_thickness, cv2.LINE_AA)
+        cv2.line(canvas, (reference_x + 1, chart_y1), (reference_x + 1, chart_y2), (255, 180, 80), ref_thickness + 1, cv2.LINE_AA)
+        cv2.line(canvas, (reference_x, chart_y1), (reference_x, chart_y2), (255, 180, 80), ref_thickness, cv2.LINE_AA)
         return canvas
 
     def _overlay_bbox_result(self, frame, roi_plotted, bbox_rect, score=None):

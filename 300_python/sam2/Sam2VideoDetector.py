7913f892db60b6079c0cb45ee7932754bf090c59
sunabove <sunabove@nate.com>
2026-08-12 15:01:37 +0900

    ㅁㅁ

--- 300_python/sam2/Sam2VideoDetector.py
+++ 300_python/sam2/Sam2VideoDetector.py
@@ -1024,19 +1024,19 @@ class Sam2VideoDetector:
 
         legend_x = chart_x1 + 8
         legend_gap = 10
-        legend_top_margin = 6
-        legend_text_y = chart_y1 + legend_top_margin
+        legend_top_margin = 8
         for legend_text, legend_color, legend_scale in legend_items:
-            (legend_width, legend_height), _legend_baseline = cv2.getTextSize(
+            (legend_width, legend_height), legend_baseline = cv2.getTextSize(
                 legend_text,
                 cv2.FONT_HERSHEY_SIMPLEX,
                 legend_scale,
                 1,
             )
+            text_origin_y = chart_y1 + legend_top_margin + legend_height + legend_baseline
             cv2.putText(
                 canvas,
                 legend_text,
-                (legend_x, legend_text_y + legend_height),
+                (legend_x, text_origin_y),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 legend_scale,
                 legend_color,

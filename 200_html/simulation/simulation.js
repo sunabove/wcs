934e1ef10fdecb86e0cbcdc4846dafacf092ef57
sunabove <sunabove@nate.com>
2026-08-19 16:12:06 +0900

    ㅁㅁ

--- 200_html/simulation/simulation.js
+++ 200_html/simulation/simulation.js
@@ -4979,7 +4979,7 @@ class RapierDriveSimulation {
     const roofOffset = new THREE.Vector3(
       Number(this.vehicleColliderLocalCenter.x) || 0,
       Number(this.vehicleColliderLocalCenter.y) || 0,
-      (Number(this.vehicleColliderLocalCenter.z) || 0) + halfZ + 0.06,
+      (Number(this.vehicleColliderLocalCenter.z) || 0) + halfZ,
     ).applyQuaternion(carQuaternion);
     const initialYaw = Number.isFinite(this.vehicleInitialYawRad)
       ? this.vehicleInitialYawRad
